217b9ef16db302903de4b5e4ab67faf8b7bc703b
Bianca-why <56407953+Bianca-why@users.noreply.github.com>
2019-10-21 15:59:48 -0400

    Beginning again to correct issue with how Copy Paste from my Notes altered Code Format

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -1,100 +1,42 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+/*** Treehouse FSJS Techdegree: project 1- A Random Quote Generator ***/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+/*** Created an array with 6 objects containing both a quote source, 
+ * and some with a year and citation--later added a occupation ***/
 
-let quotes = [
-    {
-      quote: "I am learning all the time. The tombstone will be my diploma.", 
-      source: "Eartha Kitt", 
-      occupation: "Actor"
-    },
-    {
-      quote: "Understand that the right to choose your own path is a sacred privilege. Use it. Dwell in possibility.",
-      source: "Oprah Winfrey",
-      occupation: "Actor"
-    },
-    {
-      quote: "One press account said I was an overnight success. I thought that was the longest night I’ve ever spent.",
-      source: "Sandra Cisneros",
-      citation: "Chicago Tribune",
-      year: "1992",
-      occupation: "Author"
-    },
-    {
-      quote: "I’m just enjoying my life. I suggest you try it.",
-      source: "Tyler Perry",
-      occupation: "Actor"
-    },
-    {
-      quote: "I'm not afriad of storms, for I'm learning how to sail my ship.",
-      source: "Louisa May Alcott",
-      occupation: "Author"
-    },
-    {
-      quote: "She wasn’t waiting for a knight, she was waiting for a sword.",
-      source: "Atticus",
-      year: "2015",
-      occupation: "Author"
-    }
-    ];
-    
-  //Tested array in console--objects in array are working
-    console.log(quotes);
-  
-  /***Created getRandomQuote function. Created a random number to select a random object from the array and return  a quote.
-  returning quotes variable displays quote array with the random fuction created. ***/
-  
-  function getRandomQuote () {
-    let randomNumber = Math.floor(Math.random() * quotes.length);
-    return quotes[randomNumber];
-  };
-  //tested the getRandomQuote funciton to see if I am pulling a random quote; was pulling a random number vs quote, corrected.
-  console.log(getRandomQuote());
-  
-  /***Created a printQuote function. Stored the getRandomQuote variable in the function and created a variable 
-  to store the HTML string for the quote and source, add conditional statements for year and citation properties. 
-  Set innerHTML property with the 'quote-box' div to equal the HTML string. ***/
-  
-  function printQuote () {
-      let printRandomQuote = getRandomQuote ();
-      let quoteMessage = ''; //HTML settings variable named quoteMessage
-      //Fixed printRandomQuote.quote HTML string by adding correct spaces, adding the + to concatenate
-      quoteMessage += '<p class="quote">' +printRandomQuote.quote+ '</p><p class="source">'+printRandomQuote.source;
-     //note: dot notation for each property in the objects is object.quote, object.source; object.year, and object.citation.
-  
-     //Visual appeal is for the citation to come before the year, switched order code pulls citation and year.
-    if(printRandomQuote.citation) {
-    quoteMessage += '<span class="citation">' +printRandomQuote.citation+ '</span>';
-   }
-  
-    if(printRandomQuote.year) {
-      quoteMessage += '<span class="year">' +printRandomQuote.year+ '</span>';
-     }
-  //for exceedes adding conditional statement to include occupation in last position of format.
-    if(printRandomQuote.occupation) {
-      quoteMessage += '<span class="occupation">' +printRandomQuote.occupation+ '</span>';
-     }
-  // closed quoteMessage call <p></p>
-      quoteMessage += '</p>';
-    
-    console.log(quoteMessage); 
-  
-  //set the inner HTML by targeting the div with JS
-  document.getElementById('quote-box').innerHTML = quoteMessage;
-  //correctly closed printQuote function and console.log(quoteMessage) works with quote button; still an issue with printRandomQuote
-  };
-  printQuote();
-  
-  //For exceeds added 20,000 miliseconds or 20 second timer to the printQuote function.
-  setInterval(printQuote, 20000);
-  
-  /***The included code below is triggered when a site user clicks "Show another Quote" button.
-  the event listener calls the 'prinitQuote' function created. ***/
-  
-  document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-  
-  //Original comments replaced with my comments.
-  
+ let quotes = [
+   { 
+     quote: "I am learning all the time. The tombstone will be my dipolma.",
+     source: "Eartha Kitt",
+     occupation: "Actor"
+   },
+   {
+     quote: "Understand that the right to choose your own path is a sacred privlege. Use it. Dwill in possiblity.",
+     source: "Oprah Winfrey",
+     occupation: "Actor"
+   },
+   {
+     quote: "One press account said I was an overnight successs. I thought that was the longest night I've ever spent.",
+     source: "Sandra Cisneros",
+     citation: "Chicago Tribune",
+     year: "1992",
+     occupation: "Author"
+   },
+   {
+     quote: "I'm just enjoying my life. I suggest you try it.",
+     source: "Tyler Perry",
+     occupation: "Actor"
+   },
+   {
+     quote: "I'm not afraid of storms, for I'm learning how to sail my ship.",
+     source: "Louisa May Alcott",
+     occupation: "Author"
+   },
+   {
+     quote: "She wasn't waiting for a knight, she was waiting for a sword.",
+     source: "Atticus",
+     year: "2015",
+     occupation: "Author"
+   },
+ ];
+ //Tesed array in console--objects in array are working.
+ console.log(quotes);
